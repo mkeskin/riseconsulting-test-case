f7338ad7009dd7701c52878ac2755ed3031dc7c3
Mustafa Keskin <1378380+mkeskin@users.noreply.github.com>
2023-01-17 17:51:39 +0300

    chore: Initialization with configs and directory structure designs.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./app/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      cursor: {
+        inherit: 'inherit',
+      },
+    },
+  },
+  plugins: [],
+}
