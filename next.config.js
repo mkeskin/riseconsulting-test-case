6e36d999384b947b939f981869c8a54d28e1dbe5
Mustafa Keskin <1378380+mkeskin@users.noreply.github.com>
2023-01-17 18:14:11 +0300

    fix: nextjs configs resolved.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {
-      appDir: true,
-    },
+    appDir: true,
   },
 }
 
